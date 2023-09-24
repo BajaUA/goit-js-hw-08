2086012aab9f460407258bbc4d25345ad85a5cb1
BajaUA <bajasm27@gmail.com>
2023-09-24 19:43:55 +0300

    hw-8-first-try

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe, {
+  width: 640,
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const currentTimeKey = 'videoplayer-current-time';
+
+const savedTime = localStorage.getItem(currentTimeKey);
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem(currentTimeKey, seconds);
+  }, 1000)
+);
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageTextarea = form.querySelector('textarea[name="message"]');
+
+function saveForm() {
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function savedFormState() {
+  const savedData = localStorage.getItem('feedback-form-state');
+  if (savedData) {
+    const formData = JSON.parse(savedData);
+    emailInput.value = formData.email;
+    messageTextarea.value = formData.message;
+  }
+}
+emailInput.addEventListener('input', throttle(saveForm, 500));
+messageTextarea.addEventListener('input', throttle(saveForm, 500));
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const formData = {
+    email: emailInput.value,
+    message: messageTextarea.value,
+  };
+
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageTextarea.value = '';
+
+  console.log(formData);
+});
+
+savedFormState();
